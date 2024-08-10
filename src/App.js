6da0f27b36702109c6f97f5e7e21677629c67de6
Miguel Martínez <58083872+migueljolv@users.noreply.github.com>
2024-08-10 12:21:36 -0500

    Project revamp

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function App () {
-  return (
-    <h1>Kanban</h1>
-  )
-}
-
-export default App
--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Button () {
-  return (
-    <button>
-      Button
-    </button>
-  )
-}
