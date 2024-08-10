37b6d11484d7d83ea8b88f58cc3e4d3a89ff864c
Miguel Martínez <58083872+migueljolv@users.noreply.github.com>
2024-08-10 12:44:57 -0500

    Storybook revamp

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { Button } from "./Button";
+
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    children: "Button",
+  },
+};
